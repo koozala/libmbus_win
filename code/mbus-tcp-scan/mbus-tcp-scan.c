6c5af56bd556c7395165a2a857f74d587f9532c6
koozala <harald@hvogt.net>
2019-06-28 14:25:35 +0200

    Fix missing file

--- code/mbus-tcp-scan/mbus-tcp-scan.c
+++ code/mbus-tcp-scan/mbus-tcp-scan.c
@@ -2,123 +2,170 @@
 // Copyright (C) 2011, Robert Johansson, Raditex AB
 // All rights reserved.
 //
-// rSCADA 
+// rSCADA
 // http://www.rSCADA.se
 // info@rscada.se
 //
 //------------------------------------------------------------------------------
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <string.h>
 
 #include <stdio.h>
 #include <mbus.h>
 
+static int debug = 0;
+
+int ping_address(mbus_handle* handle, mbus_frame* reply, int address)
+{
+	int i, ret = MBUS_RECV_RESULT_ERROR;
+
+	memset((void*)reply, 0, sizeof(mbus_frame));
+
+	for (i = 0; i <= handle->max_search_retry; i++)
+	{
+		if (debug)
+		{
+			printf("%d ", address);
+			fflush(stdout);
+		}
+
+		if (mbus_send_ping_frame(handle, address, 0) == -1)
+		{
+			fprintf(stderr, "Scan failed. Could not send ping frame: %s\n", mbus_error_str());
+			return MBUS_RECV_RESULT_ERROR;
+		}
+
+		ret = mbus_recv_frame(handle, reply);
+
+		if (ret != MBUS_RECV_RESULT_TIMEOUT)
+		{
+			return ret;
+		}
+	}
+
+	return ret;
+}
+
 
 //------------------------------------------------------------------------------
-// Execution starts here:
+// Primary addressing scanning of mbus devices.
 //------------------------------------------------------------------------------
 int
 main(int argc, char **argv)
 {
     mbus_handle *handle;
     char *host;
-    int port, address, debug = 0;
+    int address, retries = 0;
+    uint16_t port;
     int ret;
+
+	WSADATA wsaData;
+	int r;
+
+	r = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (r != 0) {
+		printf("Could not initialize sockets. Error=%d\n", r);
+		return 1;
+	}
 
     if (argc == 3)
     {
-        host = argv[1];   
-        port = atoi(argv[2]);
+        host = argv[1];
+        port = (uint16_t)atol(argv[2]);
     }
     else if (argc == 4 && strcmp(argv[1], "-d") == 0)
     {
         debug = 1;
-        host = argv[2];   
-        port = atoi(argv[3]);
+        host = argv[2];
+        port = (uint16_t)atol(argv[3]);
+    }
+    else if (argc == 5 && strcmp(argv[1], "-r") == 0)
+    {
+        retries = atoi(argv[2]);
+        host = argv[3];
+        port = (uint16_t)atol(argv[4]);
+    }
+    else if (argc == 6 && strcmp(argv[1], "-d") == 0 && strcmp(argv[2], "-r") == 0)
+    {
+        debug = 1;
+        retries = atoi(argv[3]);
+        host = argv[4];
+        port = (uint16_t)atol(argv[5]);
     }
     else
     {
-        printf("usage: %s [-d] host port\n", argv[0]);
+        fprintf(stderr,"usage: %s [-d] [-r RETRIES] host port\n", argv[0]);
         return 0;
+    }
+
+    if ((port < 0) || (port > 0xFFFF))
+    {
+        fprintf(stderr, "Invalid port: %ld\n", port);
+        return 1;
+    }
+
+    if ((handle = mbus_context_tcp(host, port)) == NULL)
+    {
+        fprintf(stderr,"Scan failed: Could not initialize M-Bus context: %s\n",  mbus_error_str());
+        return 1;
     }
     
     if (debug)
     {
-        mbus_register_send_event(&mbus_dump_send_event);
-        mbus_register_recv_event(&mbus_dump_recv_event);
+        mbus_register_send_event(handle, &mbus_dump_send_event);
+        mbus_register_recv_event(handle, &mbus_dump_recv_event);
     }
-     
-    if ((handle = mbus_connect_tcp(host, port)) == NULL)
+
+    if (mbus_connect(handle) == -1)
     {
-        printf("Scan failed: Could not setup connection to M-bus gateway: %s\n",  mbus_error_str());
+        fprintf(stderr,"Scan failed: Could not setup connection to M-bus gateway: %s\n", mbus_error_str());
+        return 1;
+    }
+
+    if (mbus_context_set_option(handle, MBUS_OPTION_MAX_SEARCH_RETRY, retries) == -1)
+    {
+        fprintf(stderr,"Failed to set retry count\n");
         return 1;
     }
 
     if (debug)
         printf("Scanning primary addresses:\n");
 
-    for (address = 0; address <= 250; address++)
+    for (address = 0; address <= MBUS_MAX_PRIMARY_SLAVES; address++)
     {
         mbus_frame reply;
 
-        memset((void *)&reply, 0, sizeof(mbus_frame));
+        ret = ping_address(handle, &reply, address);
 
-        if (debug)
-        {
-            printf("%d ", address);
-            fflush(stdout);
-        }
-
-        if (mbus_send_ping_frame(handle, address) == -1)
-        {
-            printf("Scan failed. Could not send ping frame: %s\n", mbus_error_str());
-            return 1;
-        } 
-        
-        ret = mbus_recv_frame(handle, &reply);
-
-        if (ret == -1)
+        if (ret == MBUS_RECV_RESULT_TIMEOUT)
         {
             continue;
         }
-        
-        if (debug)
-            printf("\n");
-        
-        if (ret == -2)
+
+        if (ret == MBUS_RECV_RESULT_INVALID)
         {
             /* check for more data (collision) */
-            while (mbus_recv_frame(handle, &reply) != -1);
-            
+            mbus_purge_frames(handle);
             printf("Collision at address %d\n", address);
-            
             continue;
-        } 
+        }
 
         if (mbus_frame_type(&reply) == MBUS_FRAME_TYPE_ACK)
         {
             /* check for more data (collision) */
-            while (mbus_recv_frame(handle, &reply) != -1)
-            {
-                ret = -2;
-            }
-    
-            if (ret == -2)
+            if (mbus_purge_frames(handle))
             {
                 printf("Collision at address %d\n", address);
-                
                 continue;
             }
-                
+
             printf("Found a M-Bus device at address %d\n", address);
         }
     }
 
     mbus_disconnect(handle);
+    mbus_context_free(handle);
     return 0;
 }
+
 
